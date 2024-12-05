d6678e33628511686a2101fd3c199fc3654891fd
Mike Goddard <mike.goddard@nimbleapproach.com>
2024-12-05 14:35:02 +0000

    Day 4 part 1 tidy up

--- src/day_4/part1.rs
+++ src/day_4/part1.rs
@@ -4,13 +4,27 @@ pub fn part1(lines: Vec<String>) -> i64 {
   let re = Regex::new(r".*?(XMAS).*?").unwrap();
   let reverse_re = Regex::new(r".*?(SAMX).*?").unwrap();
   let mut result = 0;
-  // Count horizontal matches
-  for line in &lines {
+  // Add horizonal lines
+  let mut all_lines = lines.clone();
+  // Add vertical lines
+  let vertical = get_verticals(&lines);
+  all_lines.extend(vertical);
+  // Add diagonal lines
+  let diagonal  = get_diagonals(lines.iter().map(|l| l.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>());
+  all_lines.extend(diagonal);
+  // Add opposite diagonal lines
+  let opposite_diagonal  = get_opposite_diagonals(lines.iter().map(|l| l.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>());
+  all_lines.extend(opposite_diagonal);
+  // Count matches
+  for line in &all_lines {
     result += re.captures_iter(line).count() as i64;
     result += reverse_re.captures_iter(line).count() as i64;
   }
-  // Count vertical matches
-  let mut reversed = lines.clone();
+  result
+}
+
+fn get_verticals(vector: &[String]) -> Vec<String> {
+  let mut reversed = vector.to_owned();
   reversed.reverse();
   let reversed_chars = reversed.iter().map(|l| l.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>();
   let mut transposed = vec![];
@@ -21,23 +35,7 @@ pub fn part1(lines: Vec<String>) -> i64 {
     }
     transposed.push(new_chars.into_iter().collect::<String>());
   }
-  for line in &transposed {
-    result += re.captures_iter(line).count() as i64;
-    result += reverse_re.captures_iter(line).count() as i64;
-  }
-  // Count diagonal matches
-  let diagonal  = get_diagonals(lines.iter().map(|l| l.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>());
-  for line in &diagonal {
-    result += re.captures_iter(line).count() as i64;
-    result += reverse_re.captures_iter(line).count() as i64;
-  }
-  // Count opposite diagonal matches
-  let opposite_diagonal  = get_opposite_diagonals(lines.iter().map(|l| l.chars().collect::<Vec<char>>()).collect::<Vec<Vec<char>>>());
-  for line in &opposite_diagonal {
-    result += re.captures_iter(line).count() as i64;
-    result += reverse_re.captures_iter(line).count() as i64;
-  }
-  result
+  transposed
 }
 
 fn get_diagonals(vector: Vec<Vec<char>>) -> Vec<String> {
