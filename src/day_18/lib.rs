9de4cef7a461a69013e4d4f8dbd839916fa1602e
Mike Goddard <mike.goddard@nimbleapproach.com>
2025-01-07 17:37:54 +0000

    Remove excess cloning from a_star

--- src/day_18/lib.rs
+++ src/day_18/lib.rs
@@ -78,3 +78,76 @@ pub fn a_star(map: &[Vec<Space>], target: (i64, i64)) -> Option<Vec<(i64, i64)>>
   None
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+pub struct Node2 {
+  pub position: (i64, i64),
+  pub parent: Option<Box<Node2>>,
+  pub g: i64,
+  pub h: i64,
+  pub f: i64,
+}
+
+pub fn a_star_2(map: &[Vec<Space>], target: (i64, i64)) -> Option<Vec<(i64, i64)>> {
+  let mut open_list = vec![Node2 {
+    position: (0, 0),
+    parent: None,
+    g: 0,
+    h: 0,
+    f: 0,
+  }];
+  let mut closed_list = vec![];
+  // A star algorithm to find the shortest path to the end
+  while !open_list.is_empty() {
+    let current_node = open_list.remove(0);
+    // Have we reached the end?
+    if current_node.position == target {
+      // Walk back to find score
+      let mut path = vec![];
+      let mut current = current_node;
+      while let Some(parent) = current.clone().parent {
+        path.push(current.position);
+        current = *parent;
+      }
+      path.reverse();
+      return Some(path);
+    }
+    closed_list.push(current_node.position);
+    // Generate children
+    let mut new_nodes = vec![];
+    for offset in [(-1, 0), (1, 0), (0, -1), (0, 1)].iter() {
+      let new_position = (current_node.position.0 + offset.0, current_node.position.1 + offset.1);
+      if new_position.0 < 0 || new_position.0 >= map[0].len() as i64 || new_position.1 < 0 || new_position.1 >= map.len() as i64 {
+        continue;
+      }
+      if map[new_position.1 as usize][new_position.0 as usize] == Space::Corrupted {
+        continue;
+      }
+      let new_g = current_node.g + 1;
+      let new_h = (new_position.0 - target.0).abs() + (new_position.1 - target.1).abs();
+      let new_node = Node2 {
+        position: new_position,
+        parent: Some(Box::new(current_node.clone())),
+        g: new_g,
+        h: new_h,
+        f: new_g + new_h,
+      };
+      if closed_list.iter().any(|p| *p == new_node.position) {
+        continue;
+      }
+      new_nodes.push(new_node);
+    }
+    // Process children
+    for new_node in new_nodes {
+      if let Some(i) = open_list.iter().position(|n| n.position == new_node.position) {
+        if new_node.g < open_list[i].g {
+          open_list.remove(i);
+          open_list.push(new_node);
+        }
+      } else {
+        open_list.push(new_node);
+      }
+    }
+    open_list.sort_by(|a, b| a.f.cmp(&b.f));
+  }
+  None
+}
--- src/day_18/part1.rs
+++ src/day_18/part1.rs
@@ -1,4 +1,6 @@
-use super::lib::{a_star, Space};
+use std::time::Instant;
+
+use super::lib::{a_star, a_star_2, Space};
 
 pub fn part1(lines: &[String]) -> String {
   let coordinates = lines.iter().map(|l| l.split(",").map(|s| s.parse().unwrap()).collect()).collect::<Vec<Vec<usize>>>();
@@ -13,7 +15,10 @@ pub fn part1(lines: &[String]) -> String {
   for coordinate in coordinates[0..num_bytes].iter() {
     map[coordinate[1]][coordinate[0]] = Space::Corrupted;
   }
-  if let Some(path) = a_star(&map, (width as i64 - 1, height - 1)) {
+  let now = Instant::now();
+  if let Some(path) = a_star_2(&map, (width as i64 - 1, height - 1)) {
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
     path.len().to_string()
   } else {
     panic!("No route found!");
